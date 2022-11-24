cd2f313b36e8da7e45654df94d829a0aee8b32fc
Sectly <67114413+Sectly@users.noreply.github.com>
2022-11-24 09:39:48 +0100

    1.0.8

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
-* SuchDB V1.0.7
+* SuchDB V1.0.8
 * An simple, dependency free, require & go node.js and browser database
 * Auhtor: Sectly (Sectly@sectly.online)
 * Licence: MPL-2.0, This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
